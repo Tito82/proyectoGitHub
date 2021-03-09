af6e0c13e0e80162b940c6ab7e7d360c78a8d068
Fernando Ares <fernandoareslouro@gmail.com>
2021-03-09 09:11:05 +0100

    añadimos clase alumno

---
+++ src/cursoGit/Alumno.java
@@ -0,0 +1,27 @@
+package cursoGit;
+
+public class Alumno {
+
+	private String matricula;
+	private String curso;
+	
+	public Alumno() {}
+	
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	@Override
+	public String toString() {
+		return "Alumno [matricula=" + matricula + ", curso=" + curso + "]";
+	}
+	
+}
